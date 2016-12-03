8a41350af853795a7dde57d80725fe9219b6c715
weizongwei5 <weizongwei5@gmail.com>
2016-12-04 00:30:32 +0800

    java 取keystore hascode

---
+++ aesjni/src/main/java/com/androidyuan/aesjni/SinalTool.java
@@ -0,0 +1,41 @@
+package com.androidyuan.aesjni;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+/**
+ * Created by wei on 16-12-4.
+ */
+
+public class SinalTool {
+
+    private static final String PKGNAME = "com.androidyuan.aesjniencrypt";
+
+
+    /* 1.如果包名不存在 就会报错  return -1
+    System.err:     android.content.pm.PackageManager$NameNotFoundException: [配置包名]
+    System.err:     at android.app.ApplicationPackageManager.getPackageInfo(ApplicationPackageManager.java:137)
+
+     2.如果配置包名不是当前进程的包名 那么，也不会报错
+    */
+    public static int getSignature(Context context) {
+
+        try {
+            PackageInfo packageInfo =
+                    context.
+                            getPackageManager().
+                            getPackageInfo(PKGNAME, PackageManager.GET_SIGNATURES);
+
+            Signature[] signs = packageInfo.signatures;
+            Signature sign = signs[0];
+            return sign.hashCode();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+}
--- app/src/main/java/com/androidyuan/aesjniencrypt/MainActivity.java
+++ app/src/main/java/com/androidyuan/aesjniencrypt/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 import com.androidyuan.aesjni.AESEncrypt;
+import com.androidyuan.aesjni.SinalTool;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +13,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toast(AESEncrypt.encode("1"));
+
+//        toast(AESEncrypt.encode("1"));
+
+        toast(SinalTool.getSignature(this)+"");
     }
 
 
     void toast(String str)
     {
-
         Toast.makeText(this,str, Toast.LENGTH_LONG).show();
     }
 }
