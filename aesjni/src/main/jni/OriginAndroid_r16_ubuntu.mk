bc986efdc4804a687dca167b99626867e92e7725
bruce <hide@hidden.com>
2021-02-24 15:46:36 +0800

    [del]:{remove useless codes.}

--- aesjni/src/main/jni/OriginAndroid_r16_ubuntu.mk
+++ aesjni/src/main/jni/OriginAndroid_r16_ubuntu.mk
@@ -1,16 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/BaseAndroid.mk
 
-
-LOCAL_MODULE    := JNIEncrypt
-LOCAL_SRC_FILES := $(LOCAL_PATH)/JNIEncrypt.c	\
-                    base64.c \
-                    keys_generator.c \
-					str_utils.cpp \
-                    checksignature.c \
-                    $(DEBUG_FILE_C) \
-                    debugger.c \
-                    logger.h
 LOCAL_CFLAGS   += -Wall -g -pedantic
 
 LOCAL_STATIC_LIBRARIES := sodium
